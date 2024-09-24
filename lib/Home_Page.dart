93a6682c20234c34cdd4ec4d3a0a77fd81bd252a
Admin <padariyaharsh06@gmail.com>
2024-09-24 16:08:28 +0530

    first commit

---
+++ lib/Home_Page.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:model_api/model/user.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  List<user> users = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("API"),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          final user = users[index];
+          return ListTile(
+            title: Text(user.name.first),
+            subtitle: Text(user.phone),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetchuser,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  void fetchuser() async {
+    print('fetchuser call');
+    const url = 'https://randomuser.me/api/?results=100';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    final results = json['results'] as List<dynamic>;
+    final transformed = results.map((e) {
+      final name = UserName(
+        title: e['name']['title'],
+        first: e['name']['first'],
+        last: e['name']['last'],
+      );
+      return user(
+        cell: e['cell'],
+        email: e['email'],
+        nat: e['nat'],
+        phone: e['phone'],
+        gender: e['gender'],
+        name: name,
+      );
+    }).toList();
+    setState(() {
+      users = transformed;
+    });
+    print('fetchuser comp..');
+  }
+}
